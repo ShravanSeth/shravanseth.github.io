fd5e23bf56f22f50e88debaffc6ee53ff63339c8
Shravan Seth <64133116+ShravanSeth@users.noreply.github.com>
2022-04-10 01:27:15 +0530

    Updated info

--- src/components/Home/About/about.jsx
+++ src/components/Home/About/about.jsx
@@ -1,25 +1,29 @@
 import React from 'react'
 
 export const about = () => {
-  return (
-    <section id="about">
+	return (
+		<section id="about">
 			<div class="container">
-                <br />
+				<br />
 				<h2 className='title'>Little about me...</h2>
 				<div class="row info">
 					<div class="col-sm-7">
-       <br  />
-						<p>I'm currently a IT Undergrad (3rd year) student at Institute of Engineering and Management,Kolkata. I enjoy creating things that live on the internet. I like making fun, interactive things with code. I specialize in building (and occasionally designing) exceptional digital experiences. 
-					</p>
-          </div>
-          <div class="col-sm-4">
-						<img src="https://res.cloudinary.com/shravanseth/image/upload/v1648308202/samples/dev_t1xiby.png" alt="Shravan Seth" class="img-responsive img-circle"/>
+						<br />
+						<p>I'm currently a IT Undergrad (3rd year) student at Institute of Engineering and Management,Kolkata. Young and Dynamic enthusiast with invaluable hands-on experience in
+							handling several simultaneous Application Development projects
+							(Web and App), Creative Designs and	Concept Creation.Equipped with the ability to escalate the
+							brand awareness by utilizing skills	gained working for several projects. Being able to identify opportunities, overcome objections, work under pressure, build long term relationships
+							and establishing a successful and lucrative company
+						</p>
 					</div>
-         
+					<div class="col-sm-4">
+						<img src="https://res.cloudinary.com/shravanseth/image/upload/v1648308202/samples/dev_t1xiby.png" alt="Shravan Seth" class="img-responsive img-circle" />
+					</div>
+
 				</div>
-				
-</div>
+
+			</div>
 
 		</section>
-  )
+	)
 }
