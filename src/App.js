cc2a57e98a1cd6d43d175aa4814d2fda61029673
Shravan Seth <64133116+ShravanSeth@users.noreply.github.com>
2022-06-04 14:48:14 +0530

    Added Resume

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
-import Navbar from './components/Navbar/navbar';
+
 import { footer as Footer } from './components/Footer/footer';
 
 function App() {
     return (
     <Router>
-        <Navbar/>
+   
         <Switch>
             <Route path="/">
                 <Home />
--- src/components/Home/About/about.jsx
+++ src/components/Home/About/about.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import pdf from '../../../Shravan-Seth-Resume.pdf'
 
 export const about = () => {
 	return (
@@ -9,8 +10,9 @@ export const about = () => {
 				<div class="row info">
 					<div class="col-sm-7">
 						<br />
-						<p>I'm a third-year IT Undergrad student at the Institute of Engineering and Management in Kolkata. As a young and energetic enthusiast, I have invaluable hands-on experience managing multiple concurrent Application Development projects (Web and App), Creative Designs, and Software Development. I have the ability to make things creative by utilising skills learned while working on various projects. Aside from that, my major qualities include the ability to identify opportunities, overcome objections, work under pressure, build long-term relationships, and establish a profitable company.
+						<p>I'm a third-year IT Undergrad student at the Institute of Engineering and Management in Kolkata. As a young and energetic enthusiast, I believe in innovative strategies and out-of-the-box solutions to problems. I have invaluable hands-on experience managing multiple concurrent Development projects in Web (MERN) and App (Flutter), making creative designs, and being an algorithmic coder. I have the ability to make things creative by utilising skills learned while working on various projects. Aside from that, my major qualities include the ability to identify opportunities, overcome objections, work under pressure, build long-term relationships, and establish a profitable company.
 						</p>
+						<a className='button workButton' href={pdf}>Download Resume</a>
 					</div>
 					<div class="col-sm-4">
 						<img src="https://res.cloudinary.com/shravanseth/image/upload/v1648308202/samples/dev_t1xiby.png" alt="Shravan Seth" class="img-responsive img-circle" />
