77d4f8baa579ec21f849970d95f08a7e5265ef6a
Shravan Seth <64133116+ShravanSeth@users.noreply.github.com>
2022-06-05 20:12:02 +0530

    Navbar Updated

---
+++ src/components/Navbar/navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './navbar.css'
+
+export default function Navbar(){
+    
+    return(
+        <>
+    <aside class="_social-links">
+	<ul class="_links-list">
+		<li class="_social-link"><a href="https://twitter.com/shhh_ravan"><i class="fa fa-twitter"></i></a></li>
+		<li class="_social-link"><a href="https://github.com/ShravanSeth" ><i class="fa fa-github"></i></a></li>
+		<li class="_social-link"><a href="https://www.instagram.com/shravanseth_/"><i class="fa fa-instagram"></i></a></li>
+        <li class="_social-link"><a href="https://www.linkedin.com/in/shravan-seth-0ab01a193/"><i class="fa fa-linkedin"></i></a></li>
+        <li class="_social-link"><a href="mailto:shravanseth59@gmail.com"><i class="fa fa-at"></i></a></li>
+	</ul>
+        </aside>
+    </>
+    );
+};
